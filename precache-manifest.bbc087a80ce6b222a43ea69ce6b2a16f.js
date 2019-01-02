d6ada59f713e31e6e79022393bbed719cdfa2701
Laura Colten <lauracolten5@gmail.com>
2019-01-01 23:41:24 -0600

    Updates

---
+++ precache-manifest.bbc087a80ce6b222a43ea69ce6b2a16f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7814c21b240af6d92a11",
+    "url": "/static/js/main.7814c21b.chunk.js"
+  },
+  {
+    "revision": "e1118e78d5a4cb6eb37b",
+    "url": "/static/js/1.e1118e78.chunk.js"
+  },
+  {
+    "revision": "7814c21b240af6d92a11",
+    "url": "/static/css/main.66f637e7.chunk.css"
+  },
+  {
+    "revision": "e1118e78d5a4cb6eb37b",
+    "url": "/static/css/1.7c1c1f39.chunk.css"
+  },
+  {
+    "revision": "c99a5f2cd3261abdc947ee9eb7fd87ec",
+    "url": "/index.html"
+  }
+];
